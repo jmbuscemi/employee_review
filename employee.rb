68aadb29ec9b5a6f4fd2405b38b9fd7a16a13c17
John M Buscemi <jmbuscemi@gmail.com>
2015-05-14 15:29:57 -0400

    Add default employee name, email, phone, salary

--- employee.rb
+++ employee.rb
@@ -1,3 +1,12 @@
 class Employee
+  attr_reader :name, :email, :phone_number, :salary
+
+  def initialize (name: "John Doe", email: "none@email.com",
+                  phone_number: "555-555-5555", salary: 50000)
+    @name = name
+    @email = email
+    @phone_number = phone_number
+    @salary = salary
+  end
 
 end
