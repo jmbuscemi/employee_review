2867c2244dbeec8a89201164322676172a9efbb8
John M Buscemi <jmbuscemi@gmail.com>
2015-05-14 15:29:30 -0400

    Test employee default name, email, phone, salary

--- employee_review_test.rb
+++ employee_review_test.rb
@@ -20,4 +20,19 @@ class EmployeeReviewTest < Minitest::Test
     assert_equal "Human Resources", Department.new("Human Resources").name
   end
 
+  def test_employee_has_default_name_email_phone_number_salary
+    assert_equal "John Doe", Employee.new.name
+    assert_equal "none@email.com", Employee.new.email
+    assert_equal "555-555-5555", Employee.new.phone_number
+    assert_equal 50000, Employee.new.salary
+  end
+
+  # def test_create_employee_with_name
+  #   assert_equal "Emily", Employee.new(name: "Emily").name
+  # end
+  #
+  # def test_employee_has_email
+  #   assert_equal ""
+  # end
+
 end
