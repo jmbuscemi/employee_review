2f931fb52efaefa22a268dace25f0c70a439d5c0
John M Buscemi <jmbuscemi@gmail.com>
2015-05-16 16:33:07 -0400

    Give department raise based on any criteria, using blocks.

--- department.rb
+++ department.rb
@@ -23,12 +23,6 @@ class Department
     employee_salaries.reduce(:+)
   end
 
-  def give_raises_good(amount)
-    good_employees = @employees.select {|e| e.performance_eval}
-    number_getting_raises = good_employees.count.to_f
-    good_employees.each {|e| e.give_raise(amount / number_getting_raises)}
-  end
-
   def give_raises (amount)
     raise_employees = @employees.select {|e| yield(e)}
     number_getting_raises = raise_employees.count.to_f
